5c8f87e4924ca224577d2882a99547ce9686a8ab
Andressa <andressagss21@gmail.com>
2023-12-01 21:02:33 -0300

    coverImage added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,19 @@
 const ProductCard = ({img, imageUrl, changeImage}) => {
+  
   const handleClick = () => {
     if (img !== imageUrl.bigShoe) {
       changeImage(imageUrl.bigShoe)
     }
   }
+
   return (
     <div className={`border-2 rounded-xl ${img === imageUrl ? "border-coral-red" : "border-none"} cursor-pointer max-sm:flex-1`}
       onClick={handleClick}
-    >
-      <img src={imageUrl} alt="" width={127} height={103}/>
+    > 
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+
+        <img src={imageUrl} alt="" width={127} height={103}/>
+      </div>
     </div>
   )
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,8 +3,11 @@ import { arrowRight } from "../assets/icons"
 import { shoes, statistics } from "../constants"
 import { bigShoe1 } from "../assets/images"
 import ProductCard from "../components/ProductCard"
+import { useState } from "react"
 
 const Hero = () => {
+  const [img, setImg] = useState(bigShoe1)
+
   return (
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:p-x pt-28">
@@ -30,11 +33,11 @@ const Hero = () => {
       </div>
 
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-cover bg-hero bg-center">
-        <img src={bigShoe1} alt="show collection" width={610} height={500} className="object-contain relative z-10"/>
+        <img src={img} alt="show collection" width={610} height={500} className="object-contain relative z-10"/>
 
-        <div>
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
             {shoes.map((item) => (
-              <ProductCard key={item.bigShoe} img="" imageUrl={item.bigShoe} changeImage={() => {}}/>
+              <ProductCard key={item.bigShoe} img="" imageUrl={item.bigShoe} changeImage={(item) => {setImg(item)}}/>
             ))}
         </div>
       </div>
